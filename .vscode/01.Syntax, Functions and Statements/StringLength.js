ce5583a62031e2aaeb6a6da8372313cd4003f7f6
KrumK <kkereziev@abv.bg>
2020-01-09 22:09:11 +0200

    Revert "First task."
    This reverts commit 1416141d00700f9a207eb7573c6571a5deb8cbcd.

--- .vscode/01.Syntax, Functions and Statements/StringLength.js
+++
@@ -1,7 +0,0 @@
-function solve(x,y,z){
-    let sum=x.length+y.length+z.length;
-    let avrg=Math.floor(sum/3);
-    console.log(sum);
-    console.log(avrg);
-}
-solve('chocolate', 'ice cream', 'cake');
