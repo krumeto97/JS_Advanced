f60ed10e873a275f8999f219310b8c066b805c4c
KrumK <kkereziev@abv.bg>
2020-01-11 10:31:53 +0200

    Podobrih 7ma zadacha

--- .vscode/01.Syntax, Functions and Statements/7.DayOfWeek.js
+++ .vscode/01.Syntax, Functions and Statements/7.DayOfWeek.js
@@ -1,29 +1,18 @@
-function Week(a) {
-    switch (a) {
-        case "Monday":
-            console.log(1);
-            break;
-        case "Tuesday":
-            console.log(2);
-            break;
-        case "Wednesday":
-            console.log(3);
-            break;
-        case "Thursday":
-            console.log(4);
-            break;
-        case "Friday":
-            console.log(5);
-            break;
-        case "Saturday":
-            console.log(6);
-            break;
-        case "Sunday":
-            console.log(7);
-            break;
-        default:
-            console.log('error');
-            break;
-    }
+const daysMap = {
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6,
+    "Sunday": 7
+};
+
+function Week(daysMap, day) {
+    return daysMap[day] ? daysMap[day] : "error";
 }
-Week("Sunday");
+console.log(
+    Week(daysMap, "Monday"),
+    Week(daysMap, "Tuesday"),
+    Week(daysMap, "Invalid")
+);
--- .vscode/test.js
+++
@@ -1,1 +0,0 @@
-let myMoney=prompt('Enter money');
