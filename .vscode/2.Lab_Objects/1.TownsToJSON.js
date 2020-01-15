aa8536cc1ed9d6245209f2b78f1d2cebcac0c47d
KrumK <kkereziev@abv.bg>
2020-01-15 15:13:57 +0200

    Changed

---
+++ .vscode/2.Lab_Objects/1.TownsToJSON.js
@@ -0,0 +1,10 @@
+function solve(){
+
+}
+console.log(
+    solve([
+        '| Town | Latitude | Longitude |',
+        '| Sofia | 42.696552 | 23.32601 |',
+        '| Beijing | 39.913818 | 116.363625 |'
+    ])
+);
