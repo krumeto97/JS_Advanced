64aa49c6ba7f735800af25475e0c03db90b1d79b
KrumK <kkereziev@abv.bg>
2020-02-03 21:39:50 +0200

    .

--- .vscode/5.DOM/01. Articles List/solution.js
+++ .vscode/5.DOM/01. Articles List/solution.js
@@ -1,4 +1,13 @@
 function createArticle() {
-	
+	// let title = document.getElementById('createTitle');
+	// let h3 = document.createElement('h3');
+	// let art1 = document.createElement('article');
+	// art1.appendChild(h3);
+	// title.appendChild(h3);
+	// document.appendChild(title);
+	let myDiv = document.getElementsByTagName('div')[0];
+	let par = document.createElement('p');
+	par.textContent = 'Lorem';
+	myDiv.appendChild(par);
 	//TODO...
 }
