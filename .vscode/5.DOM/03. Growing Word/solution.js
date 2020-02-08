51aa22f8b65d094d5c5314ac2418b245760f6e12
KrumK <kkereziev@abv.bg>
2020-02-08 21:51:07 +0200

    Lab done.

--- .vscode/5.DOM/03. Growing Word/solution.js
+++ .vscode/5.DOM/03. Growing Word/solution.js
@@ -1,4 +1,13 @@
-function growingWord() {
+const colorMap = ["#5B88BD", "#A40014", "#8FF897"];
 
-  //TODO...
+function growingWord() {
+  let word = document.querySelector("#exercise p");
+  if (word === null) {
+    throw new Error("No words for you.");
+  }
+  let fontSize = window.getComputedStyle(word).fontSize.replace("px", "");
+  word.style.color = colorMap[0];
+  let ele = colorMap.shift();
+  colorMap.push(ele);
+  word.style.fontSize = (fontSize === "0" ? "2" : fontSize * 2) + "px";
 }
--- .vscode/5.DOM/04. Visited Websites/solution.js
+++ .vscode/5.DOM/04. Visited Websites/solution.js
@@ -1,4 +1,19 @@
 function solve() {
-
-  //TODO...
+    const template = x => `visited ${x} times`;
+    const affiliateLinks = {
+        "SoftUni": 1,
+        "Google": 2,
+        "YouTube": 4,
+        "Wikipedia": 4,
+        "Gmail": 7,
+        "stackoverflow": 6
+    }
+    document.addEventListener("click", (e) => {
+        if (e.target.classList.contains("linkCta")) {
+            e.target.parentNode.querySelector(".linkOut")
+                //.nextElementSibling
+                .innerHTML = template(affiliateLinks[e.target.textContent.trim()]++);
+        }
+    });
 }
+solve();
