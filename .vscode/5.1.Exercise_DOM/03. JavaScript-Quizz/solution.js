0ce2af7b83c832f0ddb430373ee740a8a4865c9c
KrumK <kkereziev@abv.bg>
2020-02-10 17:08:05 +0200

    .

---
+++ .vscode/5.1.Exercise_DOM/03. JavaScript-Quizz/solution.js
@@ -0,0 +1,25 @@
+function solve() {
+  let doc = document.getElementById("quizzie");
+  let sections = document.getElementsByTagName("section");
+  const answers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+  let result = document.querySelector(".results-inner");
+  let answeredQuestions = 0;
+  let currentSection = 0;
+  doc.addEventListener("click", (e) => {
+    if (e.target.className === "answer-text") {
+      sections[currentSection].style.display = "none";
+      if (answers.includes(e.target.innerHTML)) {
+        answeredQuestions++;
+      }
+
+      currentSection++;
+      if (currentSection < answers.length) {
+        sections[currentSection].style.display = "block";
+      }
+      if (currentSection === 3) {
+        document.querySelector("#results").style.display = "block";
+        result.innerHTML = answeredQuestions === 3 ? "You are recognized as top JavaScript fan!" : `You have ${answeredQuestions} right answers`;
+      }
+    }
+  });
+}
