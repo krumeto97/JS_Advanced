287521f52de39cc9a0309b0c50fce971c491def7
¨krumeto97¨ <krumeto97@abv.bg>
2020-04-17 21:44:53 +0300

    This-exercise

---
+++ .vscode/1.1.This_Exercise/2.Fibonacci.js
@@ -0,0 +1,22 @@
+function getFibonator() {
+    let counter=0;
+
+    function fib(n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return fib(n-2) + fib(n-1);
+    }
+
+    return function (a){
+        let sum=fib(counter);
+        counter++
+        return sum;
+    }
+};
+// 1 1 2 3 5 8 13
+let f=getFibonator();
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f());
---
+++ .vscode/1.1.This_Exercise/3.HEX.js
@@ -0,0 +1,46 @@
+class Hex{
+    constructor(value){
+        this.value=value;
+    }
+
+    valueOf(){
+        return this.value;
+    }
+    
+    toString(){
+        return "0x"+this.value.toString(16).toUpperCase();
+    }
+
+    plus(b){
+        let sum=0;
+        if (typeof b==="object") {
+            sum+=b.valueOf()+this.value;
+        }else {
+            sum+=this.value+b;
+        }
+        let g=new Hex(sum)  
+        return g;
+    }
+
+    minus(b){
+        let sum=0;
+        if (typeof b==="object") {
+            sum+=this.value - b.valueOf();
+        }else {
+            sum+=this.value - b;
+        }
+        let g=new Hex(sum)  
+        return g;
+    }
+    
+    parse(b){
+        let numberPart=b.substring(2)
+        return parseInt(numberPart,16);
+    }
+}
+const h=new Hex(5);
+const b=new Hex(10);
+console.log(h.toString());
+console.log(h.valueOf()+1);
+console.log(h.plus(b).toString());
+console.log(h.parse("x0FF"));
